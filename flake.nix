3ea115ffb8d7c8967682cec7e81022c53a0e1529
Sojamann <robinbergewski@gmail.com>
2024-10-09 16:14:42 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,12 +7,16 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixgl.url = "github:nix-community/nixGL";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, nixgl, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        system = "${system}";
+        overlays = [ nixgl.overlay ];
+      };
     in {
       home = {
         user-config,
