889b6b3c567789908f1844b8a6b8b86c57c5d657
Sojamann <robinbergewski@gmail.com>
2024-10-10 14:53:40 +0200

    Update default.nix

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -1,8 +1,10 @@
-{ ... }: {
-  { pkgs, ...}: {
+{ pkgs, ...}: {
   home.file = {
     ".config/alacitty/alacritty.toml".source = ./alacritty.toml;
     ".config/alacitty/catppuccin-mocha.toml".source = ./catppuccin-mocha.toml;
   };
-}
+
+   home.packages = with pkgs; [
+    alacritty
+  ];
 }
