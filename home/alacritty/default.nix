54a587cda02bf47110f0f4fb70d3211b0009f95f
Sojamann <robinbergewski@gmail.com>
2024-10-10 14:56:24 +0200

    Update default.nix

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -1,10 +1,18 @@
-{ pkgs, ...}: {
+{ pkgs, ...}: 
+let
+  # until https://github.com/nix-community/home-manager/pull/5355 is merged
+  nixalacritty = pkgs.writeShellScriptBin "nixalacritty" ''
+    #!/bin/sh
+    nix run --impure github:nix-community/nixGL -- alacritty "$@"
+  '';
+in
+{
   home.file = {
     ".config/alacitty/alacritty.toml".source = ./alacritty.toml;
     ".config/alacitty/catppuccin-mocha.toml".source = ./catppuccin-mocha.toml;
   };
 
    home.packages = with pkgs; [
-    alacritty
+    nixalacritty
   ];
 }
