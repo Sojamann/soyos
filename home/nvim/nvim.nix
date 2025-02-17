f4f67292564e619eaf05b6a54fa0d729719cff46
Robin Bergewski <robinbergewski@gmail.com>
2025-02-17 11:44:14 +0100

    nvim: track config in seperate repo

--- home/nvim/nvim.nix
+++ home/nvim/nvim.nix
@@ -1,8 +1,10 @@
 { pkgs, ...}: {
 
-  home.file = {
-    ".config/nvim".source = ./config;
-  };
+  # nvim must be manually cloned from here:
+  # https://github.com/Sojamann/nvim
+  #
+  # due the lock file being readonly when using nix.
+  # The dependencies are still managed here!
 
   home.packages = with pkgs; [
     neovim
